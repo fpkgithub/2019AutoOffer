6e7e24516f58bc4c5fe3f268934076d5b0aded50
boy <861937354@qq.com>
2018-07-25 14:51:02 +0800

    boy 20180725

--- src/swordoffer/Thirdchapter/二叉树的镜像.java
+++ src/swordoffer/Thirdchapter/二叉树的镜像.java
@@ -9,5 +9,52 @@ public class 二叉树的镜像
     public static void main(String[] args)
     {
         //先序遍历节点,如果当前结点不为空，则交换
+        TreeNode27 root = new TreeNode27(1);
+        TreeNode27 node1 = new TreeNode27(2);
+        TreeNode27 node2 = new TreeNode27(3);
+
+        root.left = node1;
+        root.right = node2;
+
+        prePrintTree(root);
+        System.out.println();
+        mirror(root);
+        prePrintTree(root);
+    }
+
+    private static void prePrintTree(TreeNode27 res)
+    {
+        if (res == null)
+            return;
+        System.out.print(res.val + " ");
+        prePrintTree(res.left);
+        prePrintTree(res.right);
+    }
+
+    private static void mirror(TreeNode27 root)
+    {
+        //先序遍历节点,如果当前结点不为空，则交换
+        if (root == null)
+            return;
+        TreeNode27 node = root.left;
+        root.left = root.right;
+        root.right = node;
+        if (root.left != null)
+            mirror(root.left);
+        if (root.right != null)
+            mirror(root.right);
     }
 }
+
+class TreeNode27
+{
+    int val;
+    TreeNode27 left;
+    TreeNode27 right;
+
+    public TreeNode27(int val)
+    {
+        this.val = val;
+    }
+}
+
---
+++ src/swordoffer/Thirdchapter/对称的二叉树.java
@@ -0,0 +1,69 @@
+package swordoffer.Thirdchapter;
+
+/**
+ * 面试题28：对称的二叉树
+ * 题目：请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和
+ * 它的镜像一样，那么它是对称的。
+ */
+public class 对称的二叉树
+{
+
+
+    public static void main(String[] args)
+    {
+
+        TreeNode28 root = new TreeNode28(8);
+        TreeNode28 node1 = new TreeNode28(6);
+        TreeNode28 node2 = new TreeNode28(6);
+        TreeNode28 node3 = new TreeNode28(5);
+        TreeNode28 node4 = new TreeNode28(7);
+        TreeNode28 node5 = new TreeNode28(7);
+        TreeNode28 node6 = new TreeNode28(5);
+
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+        node2.right = node6;
+
+
+        boolean res = isSymmetrical(root);
+        System.out.println(res);
+    }
+
+    //思路：
+    //通过比较二叉树的前序遍历和对称前序遍历来判断二叉树是不是对称的
+    private static boolean isSymmetrical(TreeNode28 root)
+    {
+
+        if (root == null)
+            return true;
+        return isSymmetricalCore(root.left, root.right);
+
+    }
+
+    private static boolean isSymmetricalCore(TreeNode28 r1, TreeNode28 r2)
+    {
+        if (r1 == null && r2 == null)
+            return true;
+        if (r1 == null || r2 == null)
+            return true;
+        if (r1.val != r2.val)
+            return false;
+        return isSymmetricalCore(r1.left, r2.right) && isSymmetricalCore(r1.right, r2.left);
+    }
+
+}
+
+class TreeNode28
+{
+    int val;
+    TreeNode28 left;
+    TreeNode28 right;
+
+    public TreeNode28(int val)
+    {
+        this.val = val;
+    }
+}
